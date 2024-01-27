0a2838da3d6f5d0e42681684cf549133ba91d311
Wonjeong Kim <narubrown@users.noreply.github.com>
2024-01-27 19:50:11 +0900

    [Init] Spring Initializer

---
+++ src/main/java/me/snaptime/SnaptimeApplication.java
@@ -0,0 +1,13 @@
+package me.snaptime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnaptimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnaptimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/snaptime/SnaptimeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.snaptime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnaptimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
