bd7f2a6b1ed6af0e8434e70bdcc0b1b7fcdc9758
kim-wonjeong <codingshark@inu.ac.kr>
2024-01-28 14:24:10 +0900

    [Feat] 서버 경로 추가

--- src/main/java/me/snaptime/SnaptimeApplication.java
+++ src/main/java/me/snaptime/SnaptimeApplication.java
@@ -1,8 +1,13 @@
 package me.snaptime;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers =
+        {@Server(url = "https://na2ru2.me:8080/", description = "Default Server URL"),
+                @Server(url = "/", description = "Development Server URL")})
 @SpringBootApplication
 public class SnaptimeApplication {
 
