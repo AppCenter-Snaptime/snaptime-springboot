eb03d7489e2ec5bc3bc601bc2c032795c071cd2d
NARUBROWN <codingshark@inu.ac.kr>
2024-07-07 17:14:42 +0900

    [Fix] 사용하지 않는 테스트 삭제 #76

--- src/test/java/me/snaptime/snap/service/SnapServiceImplTest.java
+++ src/test/java/me/snaptime/snap/service/SnapServiceImplTest.java
@@ -58,7 +58,7 @@ public class SnapServiceImplTest {
     String testImagePath = "test_resource/image.jpg";
     ClassPathResource resource = new ClassPathResource(testImagePath);
 
-    @DisplayName("스냅 저장 테스트")
+    /*@DisplayName("스냅 저장 테스트")
     @Test
     @Transactional
     public void createSnapTest() throws Exception {
@@ -102,7 +102,7 @@ public class SnapServiceImplTest {
         given(fileComponent.writePhotoToFileSystem(givenMultipartFile.getOriginalFilename(), givenMultipartFile.getContentType(), encryptData)).willReturn(expectedWritePhotoToFileSystemResult);
         // when
         //snapServiceImpl.createSnap(givenCreateSnapReqDto, "abcd", givenPrivate);
-    }
+    }*/
 
     /*@DisplayName("스냅 가져오기 테스트")
     @Test
